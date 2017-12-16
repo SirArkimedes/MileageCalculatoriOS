2209027c4fcf616259f77f3543f1e5c29338b115
Andrew Robinson <sirarkimedes@gmail.com>
2017-12-15 23:56:10 -0600

    Add Xcode project

---
+++ MileageCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MileageCalculator
+//
+//  Created by Andrew Robinson on 12/15/17.
+//  Copyright © 2017 Andrew Robinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
